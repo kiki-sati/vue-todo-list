191725be787be553ba32c3d089cdbebe30ba2c0f
kimrumm <kimrumm@naver.com>
2023-10-25 00:40:57 +0900

    vuex 초기 세팅

---
+++ src/Constant.js
@@ -0,0 +1,6 @@
+export default {
+    // 변이의 이름은 단순한 문자열이기 때문에 오타를 내기 쉬워서 상수를 만들어 사용.
+    ADD_TODO : "addTodo",
+    DONE_TOGGLE : "doneToggle",
+    DELETE_TODO : "deleteTodo",
+}
---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.header {
+  background-color: purple;
+  padding: 30px 30px;
+  color: yellow;
+  text-align: center;
+}
+
+.header:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+<template>
+  <div id="todolistapp">
+    <div id="header" class="header">
+      <h2>Todo List App</h2>
+      <input-todo/>
+    </div>
+    <item-list></item-list>
+  </div>
+</template>
+<script type="text/javascript">
+import InputTodo from './InputTodo';
+import ItemList from "@/components/ItemList.vue";
+
+export default {
+  name      : 'todo-list',
+  components: {InputTodo, ItemList}
+}
+</script>
