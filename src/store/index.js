c771bf9bdc35e995bc0449628d1aae6598c398e2
kimrumm <kimrumm@naver.com>
2023-10-26 10:53:13 +0900

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,17 @@ import Vuex from 'vuex';
 import state from './state';
 import mutations from './mutations';
 import actions from './actions';
+import createPersistedState from "vuex-persistedstate";
+
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	state,
 	mutations,
-	actions
+	actions,
+	plugins: [
+		createPersistedState()
+	]
 })
 
 export default store;
